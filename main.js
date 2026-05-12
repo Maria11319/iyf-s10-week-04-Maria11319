5cde44f4fe9128fe1114fb222ce40fcb4c084062
Maria11319 <mariajones3197@gmail.com>
2026-05-12 20:38:31 +0300

    feat: add JS variables practice

---
+++ main.js
@@ -0,0 +1,28 @@
+// Variable declarations
+let name = "Maria";
+let age = 25;
+const birthYear = 1999;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+
+const PI = 3.14159;
+
+// Variable Practice
+let myName = "Maria";
+let myAge = 25;
+let isStudent = true;
+let favoriteColors = ["blue", "green", "purple"];
+let today = new Date();
+
+console.log("My name is: " + myName);
+console.log("My age is: " + myAge);
+console.log("Am I a student: " + isStudent);
+console.log("My favorite colors: " + favoriteColors);
+console.log("Today's date: " + today);
