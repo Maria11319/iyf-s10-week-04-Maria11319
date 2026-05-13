cf2aa33df457296068484a5cd849881a596f46bd
Maria11319 <mariajones3197@gmail.com>
2026-05-13 12:10:53 +0300

    Add Task 7.2 Number Operations

--- main.js
+++ main.js
@@ -26,3 +26,24 @@ console.log("My age is: " + myAge);
 console.log("Am I a student: " + isStudent);
 console.log("My favorite colors: " + favoriteColors);
 console.log("Today's date: " + today);
+
+console.log("=== Task 7.2: Number Operations ===");
+
+
+let a = 10;
+let b = 3;
+
+console.log(a + b );
+console.log(a - b );
+console.log(a * b );
+console.log(a / b ); // Division
+console.log(a % b ); // Modulus
+console.log(a ** b ); // Exponentiation
+
+
+let count = 0;
+console.log("count starts at:",count);
+count++;
+console.log("After count++:",count);
+count--;
+console.log("After count--:",count);
