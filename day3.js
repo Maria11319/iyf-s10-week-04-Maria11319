bb906cebcece99e82f783a7679051df28fe28429
Maria11319 <mariajones3197@gmail.com>
2026-05-14 19:09:14 +0300

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,8 @@
+function isPalindrome(str) {
+    let reversed = str.split("").reverse().join("");
+    return str === reversed;
+}   
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("racecar")); // true
